cb1b3ec9a66f60a8a3c3417880a136992f42cd4b
SaruDesu422 <gilbertv722@gmail.com>
2020-10-30 13:59:30 +0700

    secondary commit

---
+++ src/test/java/p4_group_8_repo/AppTest.java
@@ -0,0 +1,20 @@
+package p4_group_8_repo;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
